d002c1b43b6c578776f7d6e233f0058881aed555
renshimosawa <polaris2760@gmail.com>
2022-01-14 21:31:56 +0900

    first commit

---
+++ components/bases/Textbox/index.stories.tsx
@@ -0,0 +1,9 @@
+export type Props = {}
+
+const Textbox: React.FC<Props> = () => (
+  <div>
+    <h1>Tailwind</h1>
+  </div>
+)
+
+export default Textbox
---
+++ components/bases/Textbox/index.tsx
@@ -0,0 +1,9 @@
+export type Props = {}
+
+const Textbox: React.FC<Props> = () => (
+  <div>
+    <h1 className="text-5xl font-bold underline">Tailwind</h1>
+  </div>
+)
+
+export default Textbox
