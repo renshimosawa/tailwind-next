4e5352c41f00fc3d50e0a3ad0f837874092fc822
renshimosawa <polaris2760@gmail.com>
2022-03-02 21:20:43 +0900

    add scss

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  cssModules: true,
-})
+const path = require('path')
+module.exports = {
+  trailingSlash: true,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
